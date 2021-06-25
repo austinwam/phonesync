eb169f4c2f5e736c0f8f5390c764614dcb4b02c7
austin wambugu <mutonyiaustin@gmail.com>
2021-06-25 11:35:10 +0300

    c1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,13 +9,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Phone sync',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Phone sync',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: MyHomePage(),
+        );
+      },
     );
   }
 }
@@ -31,61 +37,62 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-      children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.grey,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.connected_tv,
-                          size: 30,
-                        )),
-                    Text(
-                      "connect",
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.folder,
-                          size: 30,
-                        )),
-                    Text(
-                      "files",
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.settings,
-                          size: 30,
-                        )),
-                    Text(
-                      "settings",
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                  ],
-                )
-              ],
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.qr_code,
+                            size: 30,
+                          )),
+                      Text(
+                        "connect",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.folder,
+                            size: 30,
+                          )),
+                      Text(
+                        "files",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.settings,
+                            size: 30,
+                          )),
+                      Text(
+                        "settings",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        Expanded(
+          Expanded(
             flex: 5,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -110,38 +117,56 @@ class _MyHomePageState extends State<MyHomePage> {
                 Text(
                   "Files",
                   style: Theme.of(context).textTheme.headline5,
-                )
+                ),
+                Divider(),
+                Container(height: 44.h, child: DataTable()),
               ],
-            ))
-      ],
-    ));
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
-class DAtaFiles extends StatelessWidget {
-  const DAtaFiles({Key? key}) : super(key: key);
+class DataTable extends StatelessWidget {
+  const DataTable();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [Text("file name"), Text("size"), Text("phone")],
-        ),
-        Divider(),
-        ListView.separated(
-          itemCount: 25,
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-          itemBuilder: (BuildContext context, int index) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [Text("file name"), Text("size"), Text("phone")],
-            );
-          },
-        )
-      ],
-    );
+    return DataTable2(
+        columnSpacing: 12,
+        horizontalMargin: 12,
+        columns: [
+          DataColumn2(
+            label: Text('Filename'),
+            size: ColumnSize.L,
+          ),
+          DataColumn(
+            label: Text('size'),
+          ),
+          DataColumn(
+            label: Text('transfer'),
+          ),
+          DataColumn(
+            label: Text('Column D'),
+          ),
+          DataColumn(
+            label: Text('Column NUMBERS'),
+            numeric: true,
+          ),
+        ],
+        rows: List<DataRow>.generate(
+            100,
+            (index) => DataRow(cells: [
+                  DataCell(Text('file $index')),
+                  DataCell(Text('$index mb')),
+                  DataCell(IconButton(
+                    icon: Icon(Icons.upload),
+                    onPressed: () {},
+                  )),
+                  DataCell(Text('d $index')),
+                  DataCell(Text('e $index')),
+                ])));
   }
 }
